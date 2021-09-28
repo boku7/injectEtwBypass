f4edd62168f9d983a4edce73f78336b80dcc228f
Sh0ckFR <10033649+Sh0ckFR@users.noreply.github.com>
2021-09-28 17:37:39 +0200

    Added a success message when no compilation errors

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
 x86_64-w64-mingw32-gcc -m64 -mwindows -c injectEtwBypass.c -o injectEtwBypass.o -masm=intel -Wall -fno-asynchronous-unwind-tables -nostdlib -fno-ident -Wl,-Tlinker.ld,--no-seh
+echo "Succesfully compiled, you can load 'injectEtwBypass.cna' into Cobalt-Strike and use the command 'injectEtwBypass PID'"
